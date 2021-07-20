a8e81c7432b463bc190c5a4546cd407a2bd54ab6
yonghyun123 <model9960@naver.com>
2021-07-20 22:22:06 +0900

    프로그래머스 아이디

--- ReStart/src/week30/ProgBetaTest.java
+++ ReStart/src/week30/ProgBetaTest.java
@@ -38,7 +38,7 @@ public class ProgBetaTest {
     }
 }
 
-class Solution {
+ class Solution {
     //1번 수포자 1 2 3 4 5 1 2 3 4 5
     //2번 수포자 2 1 2 3 2 4 2 5 2 1
     //3번 수포자 3 3 1 1 2 2 4 4 5 5 3 3 
