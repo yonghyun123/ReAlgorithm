ecff87a84763ca66af527c5ce4ca5856b2058923
yonghyun123 <model9960@naver.com>
2021-08-13 13:24:53 +0900

    BOJ_14225  시간초과로 인한 수정

--- ReStart/src/week33/BOJ_14225.java
+++ ReStart/src/week33/BOJ_14225.java
@@ -3,9 +3,6 @@ package week33;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.StringTokenizer;
 
 
@@ -14,19 +11,17 @@ import java.util.StringTokenizer;
 public class BOJ_14225 {
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     private static StringTokenizer st;
-    private static List<Integer> combiList,resultList;
-    private static boolean[] visited;
     private static int[] board;
+    private static boolean[] visited;
     private static int n;
     
     public static void main(String[] args) throws NumberFormatException, IOException {
         n = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
-        combiList = new ArrayList<>();
-        resultList = new ArrayList<>();
+
         
         board = new int[n];
-        visited = new boolean[n];
+        visited = new boolean[2000001];
 
         int idx = 0;
         
@@ -36,40 +31,24 @@ public class BOJ_14225 {
         }
 
         for(int i = 0; i < n; i++){
-            dfs(0, i+1);
+            dfs(i, board[i]);
         }
 
-        Collections.sort(resultList);
-
-        for(int i = 0;i < resultList.size()-1; i++){
-            if(resultList.get(i+1) - resultList.get(i) >= 2){
-                System.out.println(resultList.get(i)+1);
+        for(int i = 1; i < visited.length; i++){
+            if(!visited[i]){
+                System.out.println(i);
                 break;
             }
         }
-        
-
 
 
     }
 
-    public static void dfs(int start, int r){
-        if(combiList.size() == r){
-            int temp = 0;
-            for(int i : combiList){
-                temp += i;
-            }
-            if(!resultList.contains(temp)) resultList.add(temp);
-        }
-
-        for(int i = start; i < n; i++){
-            if(!visited[i]){
-                visited[i] = true;
-                combiList.add(board[i]);
-                dfs(i+1, r);
-                combiList.remove(combiList.size()-1);
-                visited[i] = false;;
-            }
+    public static void dfs(int start, int sum){
+        visited[sum] = true;
+        for(int i = start+1; i < n; i++){
+            dfs(i, board[i]+sum);
         }
     }
+    
 }
