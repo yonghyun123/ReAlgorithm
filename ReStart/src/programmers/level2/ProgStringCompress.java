707fa71cb491c4f675b5026633c18bc1645cd78a
yonghyun123 <model9960@naver.com>
2021-08-21 09:29:49 +0900

    문자열 압축 성공

--- ReStart/src/programmers/level2/ProgStringCompress.java
+++ ReStart/src/programmers/level2/ProgStringCompress.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class ProgStringCompress {
     public static void main(String[] args) {
         ProgStringCompress.Solution solution = new ProgStringCompress().new Solution();
-        String s= "aabbaccc";
+        String s= "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
         System.out.println(solution.solution(s));
         
     }
@@ -15,7 +15,7 @@ public class ProgStringCompress {
         List<String> stringList = new ArrayList<>();
 
         public int solution(String s) {
-            int answer = 1001;
+            int answer = s.length();
 
             for(int sliceIdx = 1; sliceIdx <= (s.length() / 2); sliceIdx++){
                 answer = Math.min(resultLength(sliceIdx, s), answer);
@@ -27,33 +27,36 @@ public class ProgStringCompress {
         public int resultLength(int sliceCnt, String s){
             
             String resultSliceStr = "";
-            for(int i = 0; i < s.length()-sliceCnt; i = i+sliceCnt){ 
-                String sliceStr = s.substring(i, i+sliceCnt);
-                if(s.length() < i+sliceCnt+sliceCnt){
-                    stringList.add(sliceStr);
-                    stringList.add(s.substring(i+sliceCnt, s.length()));
-                    continue;
-                } 
-                String otherStr = s.substring(i+sliceCnt, i+sliceCnt+sliceCnt);
-                // System.out.println("1"+sliceStr+", 2"+otherStr);
-                if(sliceStr.equals(otherStr)){
-                    stringList.add(sliceStr);
+            String beforeStr = "";
+            int cnt = 1;
+            for(int i = 0; i <= s.length()+sliceCnt; i = i+sliceCnt){ 
+                String sliceStr = "";
+                //마지막 문자열
+                if(i >= s.length()){
+                    sliceStr = "";
+                } else if(s.length() < i +sliceCnt){
+                    sliceStr = s.substring(i);
+                } else  {
+                    sliceStr = s.substring(i, i+sliceCnt);
                 }
-                else{
-                    if(!stringList.isEmpty()){
-                        resultSliceStr += (stringList.size()+1 +""+stringList.get(0));
-                        stringList.clear();
-                    } else{
-                        resultSliceStr += sliceStr;
-                    }
+                
+                System.out.println(sliceStr);
+    
+
+                if(sliceStr.equals(beforeStr)){
+                    cnt += 1;
+                } else if(cnt >= 2){
+                    resultSliceStr += cnt+beforeStr;
+                    cnt = 1;
+                }else{
+                    resultSliceStr += beforeStr;
                 }
+                
+                beforeStr = sliceStr;
             }
-            if(!stringList.isEmpty()){
-                resultSliceStr += (stringList.size()+1 +""+stringList.get(0));
-                stringList.clear();
-            }
+           
             System.out.println(resultSliceStr);
-            return resultSliceStr.length();
+            return resultSliceStr.trim().length();
         }
     }
 }
